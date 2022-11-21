f3c545c6832471491561d9d5ac5693e62571fc13
Rafael Ledo <rafaeloliveiraledo@gmail.com>
2022-11-21 00:04:21 -0300

    add gulp taks and gulp watch

--- gulpTasks/servidor.js
+++ gulpTasks/servidor.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp')
 const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
 
-function monitorarArquivos () {
+function servidor() {
   return gulp.src('build')
     .pipe(webserver({
       port: 3000,
@@ -10,7 +11,11 @@ function monitorarArquivos () {
     }))
 }
 
-function servidor (cb) {
+function monitorarArquivos(cb) {
+  watch('src/**/*.html', () => gulp.series('appHTML')())
+  watch('src/**/*.scss', () => gulp.series('appCSS')())
+  watch('src/**/*.js', () => gulp.series('appJS')())
+  watch('src/assets/imgs/**/*.*', () => gulp.series('appIMG')())
   return cb()
 }
 

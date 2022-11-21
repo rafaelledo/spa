84fc436ebd8cc3d9718b0058b5e8de800a4701ab
Rafael Ledo <rafaeloliveiraledo@gmail.com>
2022-11-20 23:29:14 -0300

    implement server
    change logo and author

--- gulpTasks/app.js
+++ gulpTasks/app.js
@@ -20,12 +20,17 @@ function appCSS () {
     .pipe(gulp.dest('build/assets/css'))
 }
 
-function appJS (cb) {
-  return cb()
+function appJS () {
+  return gulp.src('src/assets/js/**/*.js')
+    .pipe(babel({ presets: ['env'] }))
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('build/assets/js'))
 }
 
-function appIMG (cb) {
-  return cb()
+function appIMG () {
+  return gulp.src('src/assets/imgs/**/*.*')
+    .pipe(gulp.dest('build/assets/imgs'))
 }
 
 module.exports = {
--- gulpTasks/servidor.js
+++ gulpTasks/servidor.js
@@ -1,10 +1,16 @@
 const gulp = require('gulp')
+const webserver = require('gulp-webserver')
 
-function monitorarArquivos(cb) {
-  return cb()
+function monitorarArquivos () {
+  return gulp.src('build')
+    .pipe(webserver({
+      port: 3000,
+      open: false,
+      livereload: true
+    }))
 }
 
-function servidor(cb) {
+function servidor (cb) {
   return cb()
 }
 

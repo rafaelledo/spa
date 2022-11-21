20ef9e0d8e6d3043c6ccf817385afde91750166b
Rafael Ledo <rafaeloliveiraledo@gmail.com>
2022-11-20 21:39:13 -0300

    implement app.js code

--- gulpTasks/app.js
+++ gulpTasks/app.js
@@ -1,18 +1,30 @@
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')(require('sass'))
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
 
-function appHTML(cb) {
+function appHTML () {
+  return gulp.src('src/**/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'))
+}
+
+function appCSS () {
+  return gulp.src('src/assets/sass/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(uglifycss({ "uglyComments": true }))
+    .pipe(concat('app.min.css'))
+    .pipe(gulp.dest('build/assets/css'))
+}
+
+function appJS (cb) {
   return cb()
 }
 
-function appCSS(cb) {
-  return cb()
-}
-
-function appJS(cb) {
-  return cb()
-}
-
-function appIMG(cb) {
+function appIMG (cb) {
   return cb()
 }
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { series, parallel } = require('gulp')
 const gulp = require('gulp')
+
 const { appHTML, appCSS, appJS, appIMG } = require('./gulpTasks/app')
 const { depsCSS, depsFonts } = require('./gulpTasks/deps')
 const { monitorarArquivos, servidor } = require('./gulpTasks/servidor')
